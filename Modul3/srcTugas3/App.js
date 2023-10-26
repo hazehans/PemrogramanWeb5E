fd7440bc44a544aa504b3671f7a7602a069c169b
Hans Aufa <94207563+hazehans@users.noreply.github.com>
2023-10-26 08:44:31 +0700

    Add files via upload
    tugas3 itu maksud saya tugas 2

---
+++ Modul3/srcTugas3/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar"
+import Pricing from "./Pages/Pricing"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
